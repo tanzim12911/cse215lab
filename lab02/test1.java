cea9cd3c091a570caad0b3b688421f7140a2fc27
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-08-07 10:48:03 +0000

    Aug 7, 2023, 10:48 AM

---
+++ lab02/test1.java
@@ -0,0 +1,11 @@
+package lab02;
+
+public class test1 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i--;
+        }while(i > 0);
+    }
+}
