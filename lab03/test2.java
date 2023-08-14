aa9c3fabe0189c0881b7fdfc876e8b173974ea76
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-08-14 11:36:13 +0000

    Aug 14, 2023, 11:36 AM

--- lab03/test2.java
+++ lab03/test2.java
@@ -1,5 +1,15 @@
 package lab03;
 
+import java.util.Scanner;
+
 public class test2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char inp_char = sc.next().charAt(0);
+        System.out.print("Enter a string: ");
+        String inp_str = sc.nextLine();
+
+        
+    }
 }
