6c520505b3e1c271a6fcf9fb04be2a6a1f1f471c
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-08-14 11:28:20 +0000

    Aug 14, 2023, 11:28 AM

--- lab03/test1.java
+++ lab03/test1.java
@@ -1,5 +1,20 @@
 package lab03;
 
+import java.util.Scanner;
+
 public class test1 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter value of A: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter value of B: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter value of C: ");
+        double c = sc.nextDouble();
+
+        double result = (b * Math.sqrt(a)) / (Math.PI * Math.pow(c, 2));
+
+        System.out.println("Result: " + result);
+    }
 }
