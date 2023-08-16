bece1f31505bdaaae78042dca711475dd1679558
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-08-16 08:22:44 +0000

    Aug 16, 2023, 8:22 AM

--- lab03/test2.java
+++ lab03/test2.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class test2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a character: ");
-        char inp_char = sc.next().charAt(0);
+        
         System.out.print("Enter a string: ");
         String inp_str = sc.nextLine();
-
+        
+        System.out.print("Enter a character: ");
+        char inp_char = sc.next().charAt(0);
+        
         String out_str = inp_str.replace(inp_char, ' ');
         System.out.println(out_str);
     }
