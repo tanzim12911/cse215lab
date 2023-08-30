435ea0b73f2ee242ea261042d870b03ab34ded74
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-08-30 11:18:41 +0000

    Aug 30, 2023, 11:18 AM

--- lab05/test.java
+++ lab05/test.java
@@ -3,17 +3,17 @@ package lab05;
 public class test {
     public static void main(String[] args) {
         System.out.println("Printing prime numbers between 1-100: ");
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 2; i <= 100; i++) {
             System.out.println(primeNum(i));
         }
     }
 
     static int primeNum(int x) {
-        for (int i = 2; i < x; i++) {
+        boolean status = false;
+        for (int i = 2; i < Math.sqrt(x); i++) {
             if (x % i != 0)
-                return x;
+                break;
         }
-
-        return -1;
+        return x;
     }
 }
