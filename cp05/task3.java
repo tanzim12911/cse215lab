34ea4d5f993f28dfacc95e91e52b7ced421835af
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-09-04 11:31:51 +0000

    Sep 4, 2023, 11:31 AM

--- cp05/task3.java
+++ cp05/task3.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cp05;
 
 import java.util.Scanner;
 
-/**
- *
- * @author LIB602IP67
- */
 public class task3 {
 
     static double balance = 0;
