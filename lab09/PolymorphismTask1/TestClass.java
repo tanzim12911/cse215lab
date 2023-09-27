ceddea05f23f4a8b4ac81637b66944c6eb967b7f
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-09-27 11:27:46 +0000

    Sep 27, 2023, 11:27 AM

--- lab09/PolymorphismTask1/TestClass.java
+++ lab09/PolymorphismTask1/TestClass.java
@@ -6,6 +6,44 @@ public class TestClass {
         p[2] = new Flower("Daffodil", "Yellow", true, false);
         p[3] = new Herb("Bayleaf", "Brown", true, "Spring");
 
-        
+        display(p);
+        search(p, "Daffodil");
+
     }
+
+    static void display(Plant[] plants) {
+            for(Plant i : plants) {
+                if(i != null) {
+                    System.out.println(i);
+                }
+            }
+    }
+
+    static void add(Plant[] plants, Plant p) {
+        for(int i = 0; i < plants.length; i++) {
+                if(plants[i] == null) {
+                    plants[i] = p;
+                }
+        }
+    }
+
+    static void remove(Plant[] plants, String n) {
+        for(int i = 0; i < plants.length; i++) {
+                if(plants[i].getName().equals(n)) {
+                    plants[i].setName(null);
+                }
+        }
+    }
+
+    static Plant search(Plant[] plants, String n) {
+        Plant result = new Plant();
+        for(int i = 0; i < plants.length; i++) {
+                if(plants[i].getName().equals(n)) {
+                    result = plants[i];
+                }
+        }
+        return result;
+    }
+
+    
 }
