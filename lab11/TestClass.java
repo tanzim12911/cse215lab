c68a73cd8b9e5d93e76be4e9bcd68a1a36bcc2ac
tanzim12911 <113380881+tanzim12911@users.noreply.github.com>
2023-10-09 10:55:03 +0000

    Oct 9, 2023, 10:55 AM

--- lab11/TestClass.java
+++ lab11/TestClass.java
@@ -2,5 +2,13 @@ public class TestClass {
     public static void main(String[] args) {
         Circle c = new Circle(5);
         Rectangle r = new Rectangle(6, 5);
+
+        System.out.println("Circle: ");
+        c.area();
+        c.perimeter();
+
+        System.out.println("Rectangle: ");
+        r.area();
+        r.perimeter();
     }
 }
